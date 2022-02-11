0406dabecd4595023bcff098352428b9a6464fc9
kmlee78 <72758925+kmlee78@users.noreply.github.com>
2022-02-11 17:29:46 +0900

    modulize api request

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import axios from "axios";
+import { board } from "../api";
 
 export default {
   data() {
@@ -38,15 +38,12 @@ export default {
     fetchData() {
       this.loading = true;
 
-      axios
-        .get("http://localhost:3000/boards")
-        .then((res) => {
-          this.boards = res.data;
+      board
+        .fetch()
+        .then((data) => {
+          this.boards = data;
         })
-        .catch((res) => {
-          this.$router.replace("/login");
-        })
-        .finally(() => {
+        .finally((_) => {
           this.loading = false;
         });
     },
